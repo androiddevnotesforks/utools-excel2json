a2ba025b6c3888ab59c4be8c86dd26aca7fbdc4a
rui <raorui2020@foxmail.com>
2022-09-02 01:16:53 +0800

    快捷键监听调整；整合复制快捷键

--- src/views/MainView/useCopy.ts
+++ src/views/MainView/useCopy.ts
@@ -2,8 +2,8 @@
 import { storeToRefs } from 'pinia'
 import { throttle } from 'lodash-es'
 import { Message as 提示 } from '@arco-design/web-vue'
+import { use快捷键监听 } from '@MainView/MainViewModule'
 import { 用户设置存储 } from '@/store/userSetting'
-import { 获取当前 } from '@/utils/getEnv'
 
 export function use复制模块(
   结果对象: any,
@@ -13,8 +13,7 @@ export function use复制模块(
 ) {
   const { copyBtnBehavior: 复制按钮行为 } = storeToRefs(用户设置存储())
   const { copy: 复制 } = useClipboard() // 复制结果功能
-  const 组合键 = useMagicKeys()
-  // 快捷键复制结果
+
   const 快捷键复制 = throttle(async () => {
     await 仅复制()
     if (!utools) {
@@ -67,17 +66,12 @@ export function use复制模块(
     return 结果对象.结果文字?.trim() && 结果对象.状态码 === 200
   })
 
-  const 系统 = 获取当前('系统')
-  const windows和linux系统 = ref(['Windows', 'Linux', 'browser'].includes(系统))
-  const mac系统 = ref(['macOS', 'browser'].includes(系统))
-
-  // 监听复制快捷键
-  watchEffect(() => {
-    const windows和linux复制条件 = windows和linux系统 && 组合键['ctrl+shift+c']
-    const mac复制条件 = mac系统 && 组合键['command+shift+c']
-    if ((windows和linux复制条件.value || mac复制条件.value) && 要显示复制按钮.value) {
-      快捷键复制()
-    }
+  use快捷键监听({
+    复制快捷键方法: () => {
+      if (要显示复制按钮.value) {
+        快捷键复制()
+      }
+    },
   })
 
   return {
--- src/views/MainView/useShortcutKey.ts
+++ src/views/MainView/useShortcutKey.ts
@@ -1,7 +1,12 @@
 /** 快捷键 */
 import { 获取当前 } from '@/utils/getEnv'
 
-export function use快捷键(朗读操作: Function) {
+interface 快捷键方法Type {
+  语音朗读快捷键方法?: Function
+  复制快捷键方法?: Function
+}
+
+export function use快捷键监听(funObj: 快捷键方法Type) {
   const 组合键 = useMagicKeys()
   const 系统 = 获取当前('系统')
   const windows和linux系统 = ref(['Windows', 'Linux', 'browser'].includes(系统))
@@ -13,7 +18,16 @@ export function use快捷键(朗读操作: Function) {
     const mac朗读条件 = mac系统 && 组合键['command+shift+s']
     if (windows和linux朗读条件.value || mac朗读条件.value) {
       // 快捷键复制()
-      朗读操作()
+      funObj.语音朗读快捷键方法 && funObj.语音朗读快捷键方法()
+    }
+  })
+
+  // 监听复制快捷键
+  watchEffect(() => {
+    const windows和linux复制条件 = windows和linux系统 && 组合键['ctrl+shift+c']
+    const mac复制条件 = mac系统 && 组合键['command+shift+c']
+    if (windows和linux复制条件.value || mac复制条件.value) {
+      funObj.复制快捷键方法 && funObj.复制快捷键方法()
     }
   })
 }
