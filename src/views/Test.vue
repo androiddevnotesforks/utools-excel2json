7406c7297b3358961cebeb57d06afce6d4fbf26e
rui <raorui2020@foxmail.com>
2022-09-05 00:23:28 +0800

    设置存储重构(未完)

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,7 +1,10 @@
 <template>
-  <div></div>
+  <div>Test</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { 用户设置存储 } from '@/store/userSetting'
+用户设置存储()
+</script>
 
 <style scoped lang="scss"></style>
