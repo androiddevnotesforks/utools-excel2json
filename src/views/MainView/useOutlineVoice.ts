dc6f5e4b8641d8345077775a025e3f77e642781a
rui <1319495697@qq.com>
2022-09-09 16:09:02 +0800

    离线朗读添加节流

--- src/views/MainView/useOutlineVoice.ts
+++ src/views/MainView/useOutlineVoice.ts
@@ -1,3 +1,5 @@
+import { throttle } from 'lodash-es'
+
 const voice = ref<SpeechSynthesisVoice>(undefined as unknown as SpeechSynthesisVoice)
 export const 离线loading = ref(false)
 const 要读的文字 = ref('')
@@ -16,6 +18,11 @@ export function 初始化离线语音() {
     })
   }
 }
+
+const 离线朗读开始 = throttle(() => {
+  离线朗读停止()
+  开始离线朗读()
+})
 
 export function 离线朗读控制(str: string) {
   if (!str) {
@@ -36,17 +43,17 @@ export function 来个随机数(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function 离线朗读开始() {
-  if (离线朗读状态.value === 'pause') {
-    window.speechSynthesis.resume()
-  } else {
-    开始离线朗读()
-  }
+// function 离线朗读开始() {
+//   if (离线朗读状态.value === 'pause') {
+//     window.speechSynthesis.resume()
+//   } else {
+//     开始离线朗读()
+//   }
 
-  // 离线loading.value = true
-  // setTimeout(() => {
-  //   离线loading.value = false
-  //   离线朗读停止()
-  //   开始离线朗读()
-  // }, 来个随机数(300, 1000))
-}
+//   // 离线loading.value = true
+//   // setTimeout(() => {
+//   //   离线loading.value = false
+//   //   离线朗读停止()
+//   //   开始离线朗读()
+//   // }, 来个随机数(300, 1000))
+// }
