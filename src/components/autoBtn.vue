6d39eedba85b29ea504aedcab5e88c3270398963
lainbo <lainbo@foxmail.com>
2022-09-12 03:19:18 +0800

    refactor: 自动按钮样式变更

--- src/components/autoBtn.vue
+++ src/components/autoBtn.vue
@@ -1,20 +1,15 @@
 <template>
-  <cus-tooltip>
-    <template #content>
-      <p>智能切换语种「{{ props.active ? '开' : '关' }}」</p>
-    </template>
-    <button
-      bg="hover:(#f2f3f5 dark:#3d3d3d)"
-      class="select-none w-32px grid-c cursor-pointer rounded-t-4px transition-all overflow-hidden aspect-ratio-square"
-      @click="handleClick"
-    >
-      <i
-        i-simple-icons-adobeillustrator
-        class="text-19px rounded-2px overflow-hidden transition-all !duration-100"
-        :class="[props.active ? 'active_auto_btn' : 'disabled_auto_btn']"
-      />
-    </button>
-  </cus-tooltip>
+  <button
+    bg="hover:(#f2f3f5 dark:#3d3d3d) active:(#eff2fa dark:#424242)"
+    class="select-none w-32px grid-c cursor-pointer rounded-t-2px transition-all duration-100 overflow-hidden aspect-ratio-square"
+    @click="handleClick"
+  >
+    <i
+      i-simple-icons-adobeillustrator
+      class="text-19px rounded-2px overflow-hidden transition-all !duration-200"
+      :class="[props.active ? 'active_auto_btn' : 'disabled_auto_btn']"
+    />
+  </button>
 </template>
 
 <script setup lang="ts">
