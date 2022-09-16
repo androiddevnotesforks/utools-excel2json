10a1ae6d78fc26911d6fb823056435192ca43074
liangjiacheng <lainbo@foxmail.com>
2022-09-16 16:57:50 +0800

    refactor: 重构快捷键触发

--- src/views/MainView/useShortcutKey.ts
+++ src/views/MainView/useShortcutKey.ts
@@ -1,104 +1,84 @@
 /** 快捷键 */
-import { isEqual } from 'lodash-es'
+import { cloneDeep } from 'lodash-es'
 import { 获取当前 } from '@/utils/getEnv'
-
+import type { 系统类型 } from '@/types/index'
 interface 快捷键方法Type {
   语音朗读快捷键方法?: Function
   复制快捷键方法?: Function
 }
 
-interface 快捷键映射类型 {
-  [key: string]: {
+type 快捷键映射类型 = {
+  [key in 'macOS' | 'other']: {
     readonly 复制: string[]
     readonly 朗读: string[]
   }
 }
+// type 系统类型 = 'windows和linux' | 'mac'
 const { current } = useMagicKeys()
-
+const 快捷键集合: 快捷键映射类型 = {
+  other: {
+    复制: ['control', 'shift', 'x'].sort(),
+    朗读: ['control', 'shift', 's'].sort(),
+  },
+  macOS: {
+    复制: ['meta', 'shift', 'c'].sort(),
+    朗读: ['meta', 'shift', 's'].sort(),
+  },
+}
 export const 当前按下的所有键 = computed(() => {
   return Array.from(current)
 })
 
-const 快捷键集合: 快捷键映射类型 = {
-  windows和linux: {
-    复制: ['control', 'shift', 'c'],
-    朗读: ['control', 'shift', 's'],
-  },
-  mac: {
-    复制: ['meta', 'shift', 'c'],
-    朗读: ['meta', 'shift', 's'],
-  },
+function 数组排序后转字符串(arr: string[]) {
+  return cloneDeep(arr).sort().join()
 }
 
 /**
- * @param 定义的快捷键arr 要按什么组合键
- * @returns  是否是当前按下的快捷键
+ * 检查按下快捷键与该系统下所有快捷键定义的长度
+ * @param os 系统
+ * @returns 当前按下的快捷键个数，与当前系统下的所有定义的快捷键，是否有长度一样的
  */
-function 按下了定义的快捷键(定义的快捷键arr: string[]) {
-  // 全都执行相同的排序算法再判断是否相等,防止顺序不一致导致的不相等
-  return isEqual(定义的快捷键arr.sort(), 当前按下的所有键.value.sort())
+function 检查按下快捷键与定义的长度(os: 系统类型) {
+  const obj = os === 'macOS' ? 快捷键集合.macOS : 快捷键集合.other
+  const 当前系统全部快捷键arr: string[][] = Object.values(obj)
+  const 当前系统下所有快捷键长度arr = 当前系统全部快捷键arr.map(item => item.length)
+  const 按下的按键个数 = 当前按下的所有键.value.length
+  return 当前系统下所有快捷键长度arr.includes(按下的按键个数)
 }
 
-/**
- * 为了防止每次按下按键都执行判断快捷键
- * @param win和lin的快捷键
- * @param mac的快捷键
- * @returns 是否应该阻断 true:应该阻断 false:不应该阻断
- */
-function 获取是否应该阻断(win和lin的快捷键: string[], mac的快捷键: string[]): boolean {
-  const 按下按键数 = 当前按下的所有键.value?.length
-  // 没按键，直接返回false
-  if (!按下按键数) {
-    return true
+function 判断按键(fn: 快捷键方法Type, os: 系统类型) {
+  const 按键合规 = 检查按下快捷键与定义的长度(os)
+  if (!按键合规) {
+    return
   }
-  // 根据系统判断，按下的快捷键和定义的快捷键长度是否相等
-  const 系统 = 获取当前('系统')
-  const 是windows和linux系统 = ['Windows', 'Linux', 'browser'].includes(系统)
-  const 是mac系统 = ['macOS', 'browser'].includes(系统)
-  if (是windows和linux系统) {
-    const win和lin按键数不匹配 = win和lin的快捷键?.length !== 按下按键数
-    return win和lin按键数不匹配
-  } else if (是mac系统) {
-    const mac按键数不匹配 = mac的快捷键?.length !== 按下按键数
-    return mac按键数不匹配
-  } else {
-    // 不知道是啥系统，直接返回true表示阻断
-    return true
-  }
-}
+  const mac快捷键条件 = os === 'macOS' && 按键合规
+  const 其他快捷键条件 = os !== 'macOS' && 按键合规
+  const 排序后按下的键str = 数组排序后转字符串(当前按下的所有键.value)
 
-function 符合win和lin或mac条件(win和lin的keys: string[], mac的keys: string[]) {
-  // 为了防止每次按下按键都执行判断快捷键
-  if (获取是否应该阻断(win和lin的keys, mac的keys)) {
-    return false
+  if (mac快捷键条件) {
+    switch (排序后按下的键str) {
+      case 快捷键集合.macOS.复制.join():
+        fn.复制快捷键方法?.()
+        break
+      case 快捷键集合.macOS.朗读.join():
+        fn.语音朗读快捷键方法?.()
+        break
+    }
+  } else if (其他快捷键条件) {
+    switch (排序后按下的键str) {
+      case 快捷键集合.other.复制.join():
+        fn.复制快捷键方法?.()
+        break
+      case 快捷键集合.other.朗读.join():
+        fn.语音朗读快捷键方法?.()
+        break
+    }
   }
-  const 系统 = 获取当前('系统')
-  const 是windows和linux系统 = ['Windows', 'Linux', 'browser'].includes(系统)
-  const 是mac系统 = ['macOS', 'browser'].includes(系统)
-  const win和lin复制条件 = 是windows和linux系统 && 按下了定义的快捷键(win和lin的keys)
-  const mac复制条件 = 是mac系统 && 按下了定义的快捷键(mac的keys)
-  return win和lin复制条件 || mac复制条件
 }
 
 export function use快捷键监听(funObj: 快捷键方法Type) {
-  // 监听朗读快捷键
   watchEffect(() => {
-    const 是否符合系统任意条件 = 符合win和lin或mac条件(
-      快捷键集合.windows和linux.朗读,
-      快捷键集合.mac.朗读
-    )
-    if (是否符合系统任意条件) {
-      funObj.语音朗读快捷键方法 && funObj.语音朗读快捷键方法()
-    }
-  })
-
-  watchEffect(() => {
-    const 是否符合任意系统条件 = 符合win和lin或mac条件(
-      快捷键集合.windows和linux.复制,
-      快捷键集合.mac.复制
-    )
-    if (是否符合任意系统条件) {
-      funObj.复制快捷键方法 && funObj.复制快捷键方法()
-    }
+    const 系统 = 获取当前('系统') as 系统类型
+    判断按键(funObj, 系统)
   })
 }
